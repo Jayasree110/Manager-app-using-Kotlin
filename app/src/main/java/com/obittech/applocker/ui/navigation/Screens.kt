cf0b52d58a13ac3d827402fb57ed5d0d3fc647b9
Jayasree <134916660+Jayasree110@users.noreply.github.com>
2026-03-09 20:23:38 +0530

    Delete app directory

--- app/src/main/java/com/obittech/applocker/ui/navigation/Screens.kt
+++
@@ -1,7 +0,0 @@
-package com.obittech.applocker.ui.navigation
-
-sealed class Screens(val route: String) {
-    object Home : Screens("home")
-    object Settings : Screens("settings")
-    object About : Screens("about")
-}
--- app/src/main/java/com/obittech/applocker/ui/screens/lockScreen/LockRequestManager.kt
+++
@@ -1,6 +0,0 @@
-package com.obittech.applocker.ui.screens.lockScreen
-
-object LockRequestManager {
-    var pendingAppPackage: String? = null
-    var unlockCallback: (() -> Unit)? = null
-}
